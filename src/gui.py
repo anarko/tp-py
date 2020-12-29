142b8c9235b96176eb09fe92e92ea8b5384f632f
Marcos Picard <anarko@gmail.com>
2020-12-29 17:55:04 -0300

    last time changes

--- src/gui.py
+++ src/gui.py
@@ -4,6 +4,7 @@ import crud_sqlite
 ''' 
     TODO : exeptions 
            form para editar/guardar
+           regex para entrada de datos
 '''
 
 class CrudTk(tkinter.Frame):
@@ -24,43 +25,48 @@ class CrudTk(tkinter.Frame):
     def __frm_nuevo_registro__(self):
         ''' Arma el form para cargar un nuevo registro '''
         self.__vacia_form__()
-        lf = tkinter.LabelFrame(self, fg="green", font=14, text="Nuevo registro")       
+        lf = tkinter.LabelFrame(self, fg="green", font=( '', 11, 'bold'), text="Nuevo registro")       
         tkinter.Label(lf, text = "Título",width = 20).pack()
-        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        self.titulo = tkinter.Entry(lf, width = 20)
+        self.titulo.pack(padx = 5, pady = 5)   
         tkinter.Label(lf, text = "Descripcion", width = 20).pack()
-        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        self.descripcion = tkinter.Entry(lf, width = 20)
+        self.descripcion.pack(padx = 5, pady = 5)   
         tkinter.Button(lf, text = "Guardar", command=self.__guardar_registro__).pack(padx = 3, pady = 3)
         lf.pack(fill="both")
 
     def __frm_buscar_registro__(self):
         ''' Arma el form para buscar un nuevo '''
         self.__vacia_form__()
-        lf = tkinter.LabelFrame(self, fg="green", font=14, text="Buscar registro")       
+        lf = tkinter.LabelFrame(self, fg="green", font=( '', 11, 'bold'), text="Buscar registro")       
         tkinter.Label(lf, text = "Título",width = 20).pack()
-        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        self.titulo = tkinter.Entry(lf, width = 20)
+        self.titulo.pack(padx = 5, pady = 5)   
         tkinter.Label(lf, text = "Descripcion", width = 20).pack()
-        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        self.descripcion = tkinter.Entry(lf, width = 20)
+        self.descripcion.pack(padx = 5, pady = 5)   
         tkinter.Button(lf, text = "Buscar", command=self.__buscar_registro__).pack(padx = 3, pady = 3)
         lf.pack(fill="both")
 
     def __vacia_form__(self):
-        ''' destruye los elementos del form '''
+        ''' destruye los elementos del form '''        
         for cosa in self.winfo_children():
             cosa.destroy()
+        
 
     def __guardar_registro__(self):
         ''' Guarda los datos integresados en la base de datos, si existen los reemplaza '''
-        pass
+        print(self.titulo.get())
 
     def __buscar_registro__(self):
         ''' Busca uno o mas registros en la base de datos '''
         pass
 
-def main():
+
+
+if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("Crud y ques")
     CrudTk(root).pack(fill="both")
     root.mainloop()
 
-if __name__ == "__main__":
-    main()
