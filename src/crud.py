408d0c33182fd84abc83ba40f30883a197da7e25
Marcos Picard <anarko@gmail.com>
2020-12-29 12:50:29 -0300

    pep align

--- src/crud.py
+++ src/crud.py
@@ -1,22 +1,22 @@
 import sqlite
 
-class crud:
+class Crud:
     db = None
 
     def __init__( self ):
-        self.db = sqlite.slqliteConn()
+        self.db = sqlite.SlqliteConn()
         return None
     
     def guarda_registro( self, registro ):
         if  ( not isinstance(registro,dict) ):
-            raise NameError('tipo_erroneo')
+            raise TypeError('TipoRegistroError')
         
         self.db.guarda_datos(registro)
         return None
     
     def busca_registro(self,registro ):
         if  ( not isinstance(registro,dict) ):
-            raise NameError('tipo_erroneo')
+            raise TypeError('TipoRegistroError')
 
         r = self.db.busca_datos(registro)
         print(r)
@@ -24,12 +24,12 @@ class crud:
 
     def elimina_registro(self,registro):
         if  ( not isinstance(registro,dict) ):
-            raise NameError('tipo_erroneo')
+            raise TypeError('TipoRegistroError')
 
         self.db.elimina_datos(registro)
         return None
 
-a = crud()
+a = Crud()
 #a.guarda_registro({'titulo':'titulo 3','descripcion':'desc rip1',})
 a.busca_registro({'titulo':"%"})
 a.elimina_registro({'titul3o':"titulo2"})
--- src/sqlite.py
+++ src/sqlite.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-class slqliteConn:
+class SlqliteConn:
     rec = None
 
     def __init__(self, fileName="default.db"):
@@ -11,7 +11,6 @@ class slqliteConn:
             self.cur = self.rec.cursor()
             self.cur.execute('CREATE TABLE IF NOT EXISTS datos(id integer, titulo text, descripcion text, PRIMARY KEY("id" AUTOINCREMENT))')
             self.cur.execute('CREATE UNIQUE INDEX IF NOT EXISTS idx_titulo ON datos (titulo)')
-        
         except:
             print("Error accediendo/creando la base de datos")
         return None
@@ -20,8 +19,7 @@ class slqliteConn:
         campos,valores = "",""
         for cosa in d:
             campos += cosa +','
-            valores += ":"+cosa+","
-            
+            valores += ":"+cosa+"," 
         query = "INSERT INTO datos ("+campos[:-1]+") VALUES ("+valores[:-1]+")"        
         self.cur.execute(query,d)
         self.rec.commit()
@@ -32,7 +30,6 @@ class slqliteConn:
         for cosa in d:
             campos += cosa +" like :"+cosa+" OR "
             valores.append("%"+str(d.get(cosa))+"%")
-        
         query = "SELECT * FROM datos WHERE "+campos[:-4]
         self.cur.execute(query,valores)
         for x in self.cur.fetchall():
@@ -44,7 +41,6 @@ class slqliteConn:
         for cosa in d:
             campos += cosa +"=:"+cosa+" AND "
             valores.append(str(d.get(cosa)))       
-            
         query = "DELETE FROM datos WHERE "+campos[:-5]
         self.cur.execute(query,valores)
         self.rec.commit()
