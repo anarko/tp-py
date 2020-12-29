20539e124abc4049508a217d984712a6acdf9a56
Marcos Picard <anarko@gmail.com>
2020-12-29 17:21:18 -0300

    Simplest data model, added some frontend

---
+++ src/crud_sqlite.py
@@ -0,0 +1,82 @@
+import sqlite3
+
+class CrudSqlite:
+    rec = None
+
+    def __init__(self, fileName="default.db"):
+        try:
+            self.rec = sqlite3.connect(fileName)
+            self.rec.row_factory = dict_factory
+            self.cur = self.rec.cursor()
+            self.cur.execute('CREATE TABLE IF NOT EXISTS datos(id integer, titulo text, descripcion text, PRIMARY KEY("id" AUTOINCREMENT))')
+            self.cur.execute('CREATE UNIQUE INDEX IF NOT EXISTS idx_titulo ON datos (titulo)')
+        except:
+            print("Error accediendo/creando la base de datos")
+        return None
+    
+    def guarda_datos(self, registro):
+        ''' Recibe un dict como registro de datos e intenta guardarlo como nuevo, si existe actualiza título y descripción '''
+
+        if ( not self.rec ):
+            raise RuntimeError("Base de datos no prensete")
+        if ( not isinstance(registro,dict) ):
+            raise TypeError('TipoRegistroError')
+        
+        query = "INSERT INTO datos (titulo,descripcion) VALUES (:titulo,:descripcion) \
+                 ON CONFLICT(titulo) DO UPDATE SET titulo=:titulo, descripcion=:descripcion"
+        self.cur.execute(query,registro)
+        self.rec.commit()
+        return None
+    
+    def busca_datos(self,registro):
+        ''' 
+            Recibe un dict como registro de datos y busca en base a los datos del registro. 
+            Devuelve una lista de diccionarios con los registros encontrados
+        '''
+
+        if ( not self.rec ):
+            raise RuntimeError("Base de datos no prensete")
+        if ( not isinstance(registro,dict) ):
+               raise TypeError('TipoRegistroError')
+        campos,valores,resultset = "",[],[]
+        for cosa in registro:
+            campos += cosa +" like :"+cosa+" OR "
+            valores.append("%"+str(registro.get(cosa))+"%")
+        query = "SELECT * FROM datos WHERE "+campos[:-4]
+        self.cur.execute(query,valores)
+        for x in self.cur.fetchall():
+            resultset.append(x)
+        return resultset
+    
+    def elimina_datos(self,registro):
+        ''' Recibe un dict como registro de datos y elimina de la base de datos en base al registro '''
+
+        if ( not self.rec ):
+            raise RuntimeError("Base de datos no prensete")
+        if ( not isinstance(registro,dict) ):
+               raise TypeError('TipoRegistroError')           
+        campos,valores = "",[]
+        for cosa in registro:
+            campos += cosa +"=:"+cosa+" AND "
+            valores.append(str(registro.get(cosa)))  
+        query = "DELETE FROM datos WHERE "+campos[:-5]
+        self.cur.execute(query,valores)
+        self.rec.commit()
+        return None
+
+
+def dict_factory(cursor, row):
+    ''' Reemplaza el generador originael de SQLite3 por uno que devuelve un dict '''
+
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+    
+
+if __name__ == "__main__":
+    ''' test module functions '''
+
+    a = CrudSqlite()
+    a.guarda_datos({'titulo':'titulo 1','descripcion':'descrip1'})
+    print(a.busca_datos({'titulo':"%"}))
--- src/gui.py
+++ src/gui.py
@@ -1,49 +1,66 @@
-import tkinter 
+import tkinter
+import crud_sqlite
 
-def vaciar_from_frame():
-    # elimino el interior del frame para crearlo de nuevo 
-    for cosa in formFrame.winfo_children():
-           cosa.destroy()
+''' 
+    TODO : exeptions 
+           form para editar/guardar
+'''
 
+class CrudTk(tkinter.Frame):
+    ''' Extiende la clase Framne de tk para poder manejar los contenedores y contenidos del crud '''
 
+    def __init__(self, master):
+        tkinter.Frame.__init__(self, master,height=300, width=100)
+        # coneccion a la base de datos
+        self.db = crud_sqlite.CrudSqlite()
+        # Menu principal
+        self.mainmenu = tkinter.Menu(master)
+        self.mainmenu.add_command(label = "Nuevo", command= self.__frm_nuevo_registro__)  
+        self.mainmenu.add_command(label = "Buscar", command= self.__frm_buscar_registro__)
+        self.mainmenu.add_command(label = "Exit", command= master.destroy)
+        master.config(menu = self.mainmenu)     
+        self.__frm_nuevo_registro__()
 
-def nuevo_registro():
-    # ventana nueva
-    #filewin = tkinter.Toplevel(main)
-    #button = tkinter.Button(filewin, text="Do nothing button")
-    #button.pack()    
-    
-    vaciar_from_frame()
+    def __frm_nuevo_registro__(self):
+        ''' Arma el form para cargar un nuevo registro '''
+        self.__vacia_form__()
+        lf = tkinter.LabelFrame(self, fg="green", font=14, text="Nuevo registro")       
+        tkinter.Label(lf, text = "Título",width = 20).pack()
+        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        tkinter.Label(lf, text = "Descripcion", width = 20).pack()
+        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        tkinter.Button(lf, text = "Guardar", command=self.__guardar_registro__).pack(padx = 3, pady = 3)
+        lf.pack(fill="both")
 
-    label = tkinter.Label(formFrame, text = "Nuevo registro")
-    label.pack()
+    def __frm_buscar_registro__(self):
+        ''' Arma el form para buscar un nuevo '''
+        self.__vacia_form__()
+        lf = tkinter.LabelFrame(self, fg="green", font=14, text="Buscar registro")       
+        tkinter.Label(lf, text = "Título",width = 20).pack()
+        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        tkinter.Label(lf, text = "Descripcion", width = 20).pack()
+        tkinter.Entry(lf, width = 20).pack(padx = 5, pady = 5)   
+        tkinter.Button(lf, text = "Buscar", command=self.__buscar_registro__).pack(padx = 3, pady = 3)
+        lf.pack(fill="both")
 
-    my_entry = tkinter.Entry(formFrame, width = 20)
-    my_entry.insert(0,'Campo1')
-    my_entry.pack(padx = 5, pady = 5)
- 
-    my_entry2 = tkinter.Entry(formFrame, width = 15)
-    my_entry2.insert(0,'campo2')
-    my_entry2.pack(padx = 5, pady = 5)
-    
-    button1 = tkinter.Button(formFrame, text = "Guardar", command=guardar_registro )
-    button1.pack(padx = 3, pady = 3)
-    
+    def __vacia_form__(self):
+        ''' destruye los elementos del form '''
+        for cosa in self.winfo_children():
+            cosa.destroy()
 
+    def __guardar_registro__(self):
+        ''' Guarda los datos integresados en la base de datos, si existen los reemplaza '''
+        pass
 
-main = tkinter.Tk()
-main.geometry("200x150")
-mainWindow = tkinter.Frame(main)
-mainWindow.pack()
- 
-mainmenu = tkinter.Menu(mainWindow)
-mainmenu.add_command(label = "Nuevo", command= nuevo_registro)  
-mainmenu.add_command(label = "Ver", command= nuevo_registro)
-mainmenu.add_command(label = "Exit", command= main.destroy)
- 
-formFrame = tkinter.Frame(main)
-formFrame.pack(side=tkinter.RIGHT)
+    def __buscar_registro__(self):
+        ''' Busca uno o mas registros en la base de datos '''
+        pass
 
-main.title("Crud y ques")
-main.config(menu = mainmenu)
-main.mainloop()
+def main():
+    root = tkinter.Tk()
+    root.title("Crud y ques")
+    CrudTk(root).pack(fill="both")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
